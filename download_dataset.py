821f8cf554dda810eb4c7c8c08e32afb45662e64
haoenlu <haoenlu.07@gmail.com>
2024-07-11 19:27:00 -0400

    update

--- download_dataset.py
+++
@@ -1,3 +0,0 @@
-import gdown 
-
-gdown.download(url="https://drive.google.com/drive/folders/1HkPUkchVzESaqem2iqaLp6m4YR-EmJpX?usp=sharing")
---
+++ download_dataset.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+API_KEY="AIzaSyB8cfU4OqKNMOVF1-z3Hfe9pCSIRBhXOrw"
+URL="https://drive.google.com/drive/folders/1HkPUkchVzESaqem2iqaLp6m4YR-EmJpX"
+whole="${URL}?alt=media&key=${API_KEY}"
+echo $whole
