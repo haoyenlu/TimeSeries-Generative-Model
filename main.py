9c6c4dc0eda07a452410d8836d39dd10d17242b3
haoenlu <haoenlu.07@gmail.com>
2024-07-11 23:11:51 -0400

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dataset import Dataset
 if __name__ == '__main__':
     args = parse_argument()
 
-    dataset = Dataset(args.path,args.config)
+    dataset = Dataset(args.data,args.config)
     (train_data, train_label), (test_data, test_label) = dataset.get_dataset(test_patient=['P29','P30'])
 
     print(train_data.shape,test_data.shape)
