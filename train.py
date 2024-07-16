0531ece0bb2fdc16468094032209a5c77411a765
haoenlu <haoenlu.07@gmail.com>
2024-07-16 16:59:39 -0400

    update

--- train.py
+++ train.py
@@ -90,6 +90,7 @@ for epoch in tqdm(range(max_epoch)):
         real_out_adv , real_out_cls = dis_net(sequence)
 
         print(real_out_cls.size())
+        print(label.size())
 
         assert fake_sequence.size() == sequence.size(),f"fake_imgs.size(): {fake_sequence.size()} real_imgs.size(): {sequence.size()}"
 
