63ae45342b9a5159d1f4b4bab19c878178f7aeab
haoenlu <haoenlu.07@gmail.com>
2024-07-16 15:35:07 -0400

    update

--- mvnx.py
+++ mvnx.py
@@ -86,7 +86,7 @@ class PreprocessMVNX:
 
                     assert resample_data.shape == (self.config['cutoff_length'],len(self.config['features'])), f"Error while parsing MVNX data, expected shape {(len(self.config['features']),self.config['cutoff_length'])}, get shape {resample_data.shape}"
                     
-                    temp_label = np.where(self.config['tasks'] == task.upper())
+                    temp_label = np.where(all_tasks == task.upper())
 
                     if subject in test_patient: 
                         TEST_DATA.append(resample_data)
