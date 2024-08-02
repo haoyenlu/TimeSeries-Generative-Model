ab4acc52a2c17e7fee085428743563726aac90ad
haoenlu <haoenlu.07@gmail.com>
2024-08-02 13:50:31 -0400

    update

--- dataset.py
+++ dataset.py
@@ -1,23 +1,54 @@
 from torch.utils.data import Dataset
 import numpy as np
+from data_utils import FeatureWiseScaler
 
 class UpperLimbMotionDataset(Dataset):
-    def __init__(self,task_data,labels=None):
+    def __init__(self,task_data,test_task_data,task='T01',labels=None,scale_range=(0,1),mode='train'):
         super(UpperLimbMotionDataset,self).__init__()
+
+        assert mode in ['train','test'],"Only support train and test mode"
+        self.mode = mode
         
-        self.data = task_data
-        self.labels = labels
+        '''Only given specific task data'''
+        train_data = task_data[task]
+        test_data = test_task_data[task]
+
         
+        '''Scaling'''
+        scaler = FeatureWiseScaler(scale_range)
+        scaler.fit(np.concatenate([train_data,test_data],axis=0))
+        self.train = scaler.transform(train_data).astype(np.float32)
+        self.test = scaler.transform(test_data).astype(np.float32)
+
+        self.labels=labels
+        
+
+            
     
     def __len__(self) -> int:
-        return self.data.shape[0]
-    
+        # Train
+        if self.mode == 'train':
+            return self.train.shape[0]
+        
+        # Test
+        else:
+            return self.test.shape[0]
+        
 
     def __getitem__(self, index):
-        if self.labels is not None:
-            return self.data[index,:,:] , self.labels[index,:]
+        # Train
+        if self.mode =='train':
+            if self.labels is not None:
+                return self.train[index,:,:] , self.labels[index,:]
+            else:
+                return self.train[index,:,:], None
+            
+        # Test
         else:
-            return self.data[index,:,:]
+            if self.labels is not None:
+                return self.test[index,:,:] , self.labels[index,:]
+            else:
+                return self.test[index,:,:], None
     
 
 
--- preprocess.py
+++ preprocess.py
@@ -8,38 +8,31 @@ from data_utils import FeatureWiseScaler
 
 from argument import preprocess_argument
 
-args = preprocess_argument()
-
-with open(args.config) as file:
-    config = yaml.safe_load(file)
 
 
-if args.mvnx is not None:
+
+def main():
+    args = preprocess_argument()
+
+    with open(args.config) as file:
+        config = yaml.safe_load(file)
+
+
+    assert args.mvnx is not None
+
     preprocessor = PreprocessMVNX(config)
-    (train_data,train_label) , (test_data,test_label) = preprocessor.get_dataset(args.mvnx,args.test_patient)
+    task_data, test_task_data = preprocessor.get_dataset(args.mvnx,args.test_patient)   
 
-    save_path = f'./{args.save}/ulf_original_{"_".join(args.test_patient)}.npy'
-    save_to_numpy(train_data,train_label,test_data,test_label,save_path)
+    save_path = f'./{args.save}/ulf_all_task.npy'
+    np.save(task_data,save_path)
 
-elif args.numpy is not None:
-    (train_data,train_label) , (test_data,test_label) = load_numpy_data(args.numpy)
-
-else:
-    raise Exception("Specify either mvnx or numpy file to load.")
-
-
-'''Scaling'''
-scale_range = config['scale_range']
-scaler = FeatureWiseScaler(scale_range)
-scaler.fit(np.concatenate([train_data,test_data],axis=0))
-X_train = scaler.transform(train_data).astype(np.float32)
-X_test = scaler.transform(test_data).astype(np.float32)
-
-
-
-save_path = f'./{args.save}/ulf_preprocess_{"_".join(args.test_patient)}.npy'
-save_to_numpy(X_train,train_label,X_test,test_label,save_path)
+    save_path = f'./{args.save}/ulf_all_task{'_'.join(args.test_patient)}.npy'
+    np.save(test_task_data,save_path)
 
 
 
 
+
+
+if __name__ == '__main__':
+    main()
