1c9336ac705ba7ee25eb55cd4e545b9c223b99ca
haoenlu <haoenlu.07@gmail.com>
2024-08-02 13:56:33 -0400

    update

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import argparse
 import yaml
 import numpy as np
+import os
 
 from mvnx import PreprocessMVNX
 from utils import save_to_numpy, load_numpy_data
@@ -20,12 +21,16 @@ def main():
 
     assert args.mvnx is not None
 
+    os.makedirs(args.save,exist_ok=True)
+
     preprocessor = PreprocessMVNX(config)
     task_data, test_task_data = preprocessor.get_dataset(args.mvnx,args.test_patient)   
 
+    # Train data
     save_path = f'./{args.save}/ulf_all_task.npy'
     np.save(task_data,save_path)
 
+    # Test Data
     save_path = f'./{args.save}/ulf_all_task{"_".join(args.test_patient)}.npy'
     np.save(test_task_data,save_path)
 
