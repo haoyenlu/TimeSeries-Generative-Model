e0cd9fa0cba21072c5e9c885cdc29834f3297553
haoenlu <haoenlu.07@gmail.com>
2024-08-05 18:33:48 -0400

    update

--- argument.py
+++ argument.py
@@ -12,7 +12,7 @@ def train_argument():
     parser.add_argument('--log',type=str,default="./log")
     parser.add_argument('--ckpt',type=str,default='./checkpoint')
     parser.add_argument('--load_ckpt',type=str,default=None)
-    parser.add_argument('--task',type=str,default='T01')
+    parser.add_argument('--task',nargs='+',stype=str)
     parser.add_argument('--curr_date',type=str,default=None)
     parser.add_argument('--save',type=str)
     
--- train.py
+++ train.py
@@ -6,9 +6,9 @@ import os
 from datetime import datetime
 import numpy as np
 
-from utils import load_numpy_data, load_config
+from utils import load_config
 from dataset import UpperLimbMotionDataset
-from model_utils import weight_init, get_trainer_from_config
+from model_utils import  get_trainer_from_config
 from train_utils import LinearLrDecay
 
 from analysis_utils import plot_pca, plot_tsne, plot_umap, plot_sample
@@ -25,50 +25,50 @@ def main():
     args = train_argument()
     config = load_config(args.config)
 
-    '''Load Data'''
-    train_dataset = UpperLimbMotionDataset(args.data,args.task)
-    train_dataloader = DataLoader(train_dataset,config['batch_size'],shuffle=True) 
-    
     if args.curr_date is None:
         curr_date = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
     else:
         curr_date = args.curr_date
 
 
-    trainer = get_trainer_from_config(args,config,curr_date)
+    for task in args.task:
+        '''Load Data'''
+        train_dataset = UpperLimbMotionDataset(args.data,task)
+        train_dataloader = DataLoader(train_dataset,config['batch_size'],shuffle=True) 
+        
 
-    # load checkpoint
-    if args.load_ckpt is not None:
-        trainer.load_weight(args.load_ckpt)
+        trainer = get_trainer_from_config(args,config,curr_date)
 
-    # logger
-    log = os.path.join(args.log,curr_date)
-    os.makedirs(log,exist_ok=True)
-    writer = SummaryWriter(log)
-    
-    # Learning rate scheduler
-    scheduler = LinearLrDecay(trainer.optimizer,config['optimizer']['lr'],0.0,0,args.max_iter)
+        # load checkpoint
+        if args.load_ckpt is not None:
+            trainer.load_weight(args.load_ckpt)
 
-
-    # training
-    trainer.train(train_dataloader,args.max_iter,args.save_iter,scheduler,writer)
-
-
-    # generate samples
-    samples = trainer.generate_samples(num_samples=100,num_per_batch=10)
-    save_path = os.path.join(args.save,curr_date,args.task)
-    os.makedirs(save_path,exist_ok=True)
-
-    np.save(os.path.join(save_path,'synthesize.npy'), samples)
+        # logger
+        log = os.path.join(args.log,curr_date,task)
+        os.makedirs(log,exist_ok=True)
+        writer = SummaryWriter(log)
+        
+        # Learning rate scheduler
+        scheduler = LinearLrDecay(trainer.optimizer,config['optimizer']['lr'],0.0,0,args.max_iter)
 
 
-    train_data = train_dataset._get_numpy()
+        # training
+        trainer.train(train_dataloader,args.max_iter,args.save_iter,scheduler,writer)
 
 
-    plot_pca(real=train_data,fake=samples,save_path=save_path)
-    plot_tsne(real=train_data,fake=samples,save_path=save_path)
-    plot_umap(real=train_data,fake=samples,save_path=save_path)
-    plot_sample(real=train_data,fake=samples,save_path=save_path)
+        # generate samples
+        samples = trainer.generate_samples(num_samples=100,num_per_batch=10)
+        save_path = os.path.join(args.save,curr_date,task)
+        os.makedirs(save_path,exist_ok=True)
+        np.save(os.path.join(save_path,'synthesize.npy'), samples)
+
+        # analyze with pca and tsne
+        train_data = train_dataset._get_numpy()
+
+        plot_pca(real=train_data,fake=samples,save_path=save_path)
+        plot_tsne(real=train_data,fake=samples,save_path=save_path)
+        plot_umap(real=train_data,fake=samples,save_path=save_path)
+        plot_sample(real=train_data,fake=samples,save_path=save_path)
 
 if __name__ == '__main__':
     main()
