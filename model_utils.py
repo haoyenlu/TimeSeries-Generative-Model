0ba08fedd3c87d0c060bdf10cb5e565b10f034e7
haoenlu <haoenlu.07@gmail.com>
2024-08-13 17:07:41 -0400

    update

--- model_utils.py
+++ model_utils.py
@@ -10,7 +10,7 @@ from models.trainer import cGANTrainer, DiffusionTrainer, ClassifyTrainer
 from models.generative.GAN import tts_cgan, eeg_cgan
 from models.generative.diffusion import diffusion_ts , unet1d
 from models.generative.diffusion.transformer import Transformer
-from models.classification.model import InceptionTime
+from models.classification.model import InceptionTime, BasicLSTM
 
 
 def get_trainer_from_config(args,config):
@@ -64,10 +64,14 @@ def get_trainer_from_config(args,config):
 
         if config['model'] == 'inception-time':
             model = InceptionTime(**config.get('inception-time',dict()))
-            optimizer = torch.optim.Adam(model.parameters(),**config.get('optimizer',dict()))
-            criterion = torch.nn.CrossEntropyLoss()
 
-            trainer = ClassifyTrainer(model,optimizer,criterion,config['num_classes'])
+        
+        if config['model'] == 'basicLSTM':
+            model = BasicLSTM(**config.get('BasicLSTM',dict()))
+
+        optimizer = torch.optim.Adam(model.parameters(),**config.get('optimizer',dict()))
+        criterion = torch.nn.CrossEntropyLoss()
+        trainer = ClassifyTrainer(model,optimizer,criterion,config['num_classes'])
 
 
     else:
@@ -93,50 +97,3 @@ def weight_init(model,init_type='normal'):
     elif classname.find('BatchNorm2d') != -1:
         nn.init.normal_(model.weight.data, 1.0, 0.02)
         nn.init.constant_(model.bias.data, 0.0)
-
-
-def generate_samples_gan(model,num_samples=1000,sample_per_batch=10):
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        samples = []
-        labels = []
-
-        latent_dim = model.latent_dim
-        num_classes = model.num_classes
-
-        model.to(device)
-        model.eval()
-
-        cnt = 0
-        while cnt < num_samples:
-            num_per_batch = min(sample_per_batch,num_samples - cnt)
-            noise = torch.FloatTensor(np.random.normal(0,1,(num_per_batch,latent_dim))).to(device)
-            fake_label = torch.randint(0,num_classes,(num_per_batch,)).to(device)
-            fake_sequence = model(noise,fake_label).to('cpu').detach().numpy()
-            samples.append(fake_sequence)
-            labels.append(fake_label.to('cpu').detach().numpy())
-            cnt += num_per_batch
-
-        return np.concatenate(samples,axis=0), np.concatenate(labels,axis=0)
-
-def generate_samples_diffusion(model,num_samples=1000,sample_per_batch=10):
-        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        sample_arr = []
-        label_arr = []
-
-        model.to(device)
-        model.eval()
-
-        pbar = tqdm(total=num_samples)
-        cnt = 0
-        while cnt < num_samples:
-            num_per_batch = min(sample_per_batch,num_samples - cnt)
-            samples  = model.generate_mts(batch_size=num_per_batch)
-            samples = samples.to('cpu').detach().numpy()
-            # labels = labels.to('cpu').detach().numpy()
-
-            sample_arr.append(samples)
-            # label_arr.append(labels)
-            cnt += num_per_batch
-            pbar.update(num_per_batch)
-
-        return np.concatenate(sample_arr,axis=0)
--- models/classification/model.py
+++ models/classification/model.py
@@ -3,82 +3,76 @@ import torch.nn as nn
 
 
 class InceptionModule(nn.Module):
-  def __init__(self,input_dim,filter_size=32,kernels=[10,20,40],use_bottleneck=True,dropout_rate=0.2):
-    super(InceptionModule,self).__init__()
-    self.bottleneck_size = filter_size
-    self.use_bottleneck = use_bottleneck
-    self.filter_size = filter_size
-    self.input_inception = nn.Conv1d(input_dim,self.bottleneck_size,kernel_size=1,padding='same',bias=False)
+	def __init__(self,input_dim,filter_size=32,kernels=[10,20,40],use_bottleneck=True,dropout_rate=0.2):
+		super(InceptionModule,self).__init__()
+		self.bottleneck_size = filter_size
+		self.use_bottleneck = use_bottleneck
+		self.filter_size = filter_size
+		self.input_inception = nn.Conv1d(input_dim,self.bottleneck_size,kernel_size=1,padding='same',bias=False)
 
-    self.conv_list = []
-    prev = input_dim if not use_bottleneck else self.bottleneck_size
+		self.conv_list = []
+		prev = input_dim if not use_bottleneck else self.bottleneck_size
 
-    for kernel in kernels:
-      self.conv_list.append(nn.Conv1d(prev,filter_size,kernel_size=kernel,padding='same',bias=False))
+		for kernel in kernels:
+			self.conv_list.append(nn.Conv1d(prev,filter_size,kernel_size=kernel,padding='same',bias=False))
 
-    self.conv_list = nn.ModuleList(self.conv_list)
+		self.conv_list = nn.ModuleList(self.conv_list)
 
-    self.max_pool_1 = nn.MaxPool1d(kernel_size=3,padding=1,stride=1)
-    self.conv6 = nn.Conv1d(input_dim,self.filter_size,kernel_size=1,padding='same',bias=False)
+		self.max_pool_1 = nn.MaxPool1d(kernel_size=3,padding=1,stride=1)
+		self.conv6 = nn.Conv1d(input_dim,self.filter_size,kernel_size=1,padding='same',bias=False)
 
-    # self.lstm = nn.LSTM(input_dim,filter_size,num_layers=2,batch_first=True)
-
-    self.bn = nn.BatchNorm1d((len(kernels) + 1) * filter_size)
-    self.act = nn.GELU()
-    self.dropout = nn.Dropout(dropout_rate)
+		self.bn = nn.BatchNorm1d((len(kernels) + 1) * filter_size)
+		self.act = nn.GELU()
+		self.dropout = nn.Dropout(dropout_rate)
 
 
-  def forward(self,x): # NCL
-    _x = x
-    if self.use_bottleneck:
-      x = self.input_inception(x)
+	def forward(self,x): # NCL
+		_x = x
+		if self.use_bottleneck:
+			x = self.input_inception(x)
 
-    x_list = []
-    for conv in self.conv_list:
-      x_list.append(conv(x))
+		x_list = []
+		for conv in self.conv_list:
+			x_list.append(conv(x))
 
-    # lstm_x, (_,_) = self.lstm(_x.permute((0,2,1)))
-    # lstm_x = lstm_x.permute((0,2,1))
 
-    _x = self.max_pool_1(_x)
-    x_list.append(self.conv6(_x))
+		_x = self.max_pool_1(_x)
+		x_list.append(self.conv6(_x))
 
-    # x_list.append(lstm_x)
+		x = torch.concat(x_list,dim=1) 
+		x = self.bn(x)
+		x = self.dropout(x)
+		x = self.act(x)
 
-    x = torch.concat(x_list,dim=1) 
-    x = self.bn(x)
-    x = self.dropout(x)
-    x = self.act(x)
-
-    return x
+		return x
 
 class ResidualLayer(nn.Sequential):
-  def __init__(self,input_dim,output_dim):
-    super(ResidualLayer,self).__init__()
-    self.conv = nn.Conv1d(input_dim,output_dim,kernel_size=1,padding='same',bias=False)
-    self.bn = nn.BatchNorm1d(output_dim)
-    self.act = nn.ReLU()
+	def __init__(self,input_dim,output_dim):
+		super(ResidualLayer,self).__init__()
+		self.conv = nn.Conv1d(input_dim,output_dim,kernel_size=1,padding='same',bias=False)
+		self.bn = nn.BatchNorm1d(output_dim)
+		self.act = nn.ReLU()
 
-  def forward(self,residual_input,input):
-    residual = self.conv(residual_input)
-    residual = self.bn(residual) 
+	def forward(self,residual_input,input):
+		residual = self.conv(residual_input)
+		residual = self.bn(residual) 
 
-    x = residual + input
-    x = self.act(x)
-    return x
-  
+		x = residual + input
+		x = self.act(x)
+		return x
+	
 
 
 class FCNLayer(nn.Module):
-  def __init__(self,input_dim,output_dim,kernel_size,stride=1,padding=1):
-    super(FCNLayer,self).__init__()
-    self.model = nn.Sequential( 
-      nn.Conv1d(input_dim,output_dim,kernel_size,stride=stride,padding=padding),
-      nn.BatchNorm1d(output_dim),
-      nn.ReLU())
-  
-  def forward(self,x):
-    return self.model(x)
+	def __init__(self,input_dim,output_dim,kernel_size,stride=1,padding=1):
+		super(FCNLayer,self).__init__()
+		self.model = nn.Sequential( 
+		nn.Conv1d(input_dim,output_dim,kernel_size,stride=stride,padding=padding),
+		nn.BatchNorm1d(output_dim),
+		nn.ReLU())
+	
+	def forward(self,x):
+		return self.model(x)
 
 
 
@@ -150,7 +144,6 @@ class InceptionTime(nn.Module):
                 s_index += 1
 
         x = self.fcn(x)
-        # x = torch.mean(x,dim=2) # NCL -> NC (average pooling)
         x = torch.flatten(x,start_dim=1)
         x = self.dropout(x)
         x = self.out(x)
@@ -158,3 +151,22 @@ class InceptionTime(nn.Module):
 
         return x
   
+
+
+class BasicLSTM(nn.Module):
+	def __init__(self,sequence_len,feature_size,num_classes,
+				hidden_size,num_layers,
+				fcn_filter):
+		super(BasicLSTM,self).__init__()
+		self.lstm = nn.LSTM(feature_size,hidden_size,num_layers,batch_first=True)
+		self.fcn = FCNLayer(hidden_size,fcn_filter,5,stride=2,padding=2)
+		self.out = nn.Linear(fcn_filter * (sequence_len // 2),num_classes)
+		self.softmax = nn.Softmax(dim=1)
+
+
+	def forward(self,x): # input shape: (N,L,C)
+		x = self.lstm(x)
+		x = torch.flatten(x,start_dim=1)
+		x = self.out(x)
+		x = self.softmax(x)
+		return x
