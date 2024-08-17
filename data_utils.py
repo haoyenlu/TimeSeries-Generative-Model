45b94563a177588841d61b276e03293231adbc8e
haoenlu <haoenlu.07@gmail.com>
2024-08-16 23:59:03 -0400

    update

--- data_utils.py
+++ data_utils.py
@@ -1,12 +1,19 @@
 import numpy as np
+from typing import Optional , Tuple, Union
+from dtaidistance import dtw_barycenter
 
-EPS = 1e-18
+import logging
+
+
+logger = logging.getLogger("augmentation")
+logger.setLevel(logging.DEBUG)
+
 
 
 class FeatureWiseScaler:
     def __init__(self,feature_range = (0,1)):
         # assert len(feature_range) == 2
-
+        self.EPS = 1e-18
         self.min_v, self.max_v = feature_range
     
     def fit(self,X):
@@ -22,9 +29,52 @@ class FeatureWiseScaler:
         return self
 
     def transform(self,X):
-        return ((X-self.mins) / (self.maxs - self.mins + EPS)) * (self.max_v - self.min_v) + self.min_v
+        return ((X-self.mins) / (self.maxs - self.mins + self.EPS)) * (self.max_v - self.min_v) + self.min_v
     
 
     def fit_transform(self,X):
         self.fit(X)
         return self.transform(X)
+    
+
+
+class DTWBarycentricAveraging:
+    def __init__(self):
+        super(DTWBarycentricAveraging, self).__init__()
+
+    def generate(
+        self,
+        X: np.ndarray,
+        n_samples: int = 1,
+        **kwargs,
+    ) -> np.ndarray :
+
+        # Draw random sample from the dataset
+        random_samples = np.random.choices(range(X.shape[0]), k=n_samples)
+        initial_timeseries = X[random_samples]
+
+        self._dtwba(
+                X_subset=X,
+                n_samples=n_samples,
+                initial_timeseries=initial_timeseries,
+                **kwargs,
+            )
+
+    def _dtwba(
+        self,
+        X_subset: np.ndarray,
+        n_samples: int,
+        initial_timeseries: Optional[np.ndarray],
+        **kwargs,
+    ) -> np.ndarray:
+        samples = []
+        for i, st in enumerate(initial_timeseries):
+            samples.append(
+                dtw_barycenter.dba(
+                    s=X_subset,
+                    c=st,
+                    nb_initial_samples=n_samples,
+                    **kwargs,
+                )
+            )
+        return np.array(samples)
---
+++ pipeline.py
@@ -0,0 +1,61 @@
+import numpy as np
+import argparse
+
+from collections import defaultdict
+
+from data_utils import FeatureWiseScaler , DTWBarycentricAveraging
+from analysis_utils import plot_pca, plot_tsne, plot_umap
+
+# Argument
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data',type=str)
+parser.add_argument('--test_patient','-tp',nargs='+',type=str)
+parser.add_argument('--include_healthy','-ih',action='store_true')
+parser.add_argument('--save','-s',type=str)
+args = parser.parse_args()
+
+
+# TODO: preprocess dataset
+# Data Structure - Healthy - H01 ~ H05 - TASK- data
+#                - Stroke - P02 ~ P30 - TASK - data
+
+
+data = np.load(args.data,allow_pickle=True).item()
+
+
+train_dataset = defaultdict(list)
+test_dataset = defaultdict(list)
+
+for type, type_dict in data.items():
+    if not args.include_healthy and type == 'Healthies': continue
+
+    for patient , patient_dict in type_dict.items():
+        for task , task_data in patient_dict.items():
+            if patient not in args.test_patient:
+                train_dataset[task].append(task_data)
+            else:
+                test_dataset[task].append(task_data)
+
+
+# TODO: Data augmentation and Preprocessing
+scaler = FeatureWiseScaler(feature_range=(0,1))
+DTW = DTWBarycentricAveraging()
+tasks = train_dataset.keys()
+
+for task in tasks:
+    train_data = scaler.fit_transform(train_dataset[task])
+    train_data_aug = DTW.generate(train_data, n_samples=100)
+
+    plot_pca(real=train_data,fake=train_data_aug,save_path=args.save)
+    plot_tsne(real=train_data,fake=train_data_aug,save_path=args.save)
+    plot_umap(real=train_data,fake=train_data_aug,save_path=args.save)
+
+
+
+
+# TODO: train generative model on train_dataset
+
+
+
+    
