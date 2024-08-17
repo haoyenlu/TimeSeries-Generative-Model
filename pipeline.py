008238122f40dd985e86940c2b58b467bfb7b822
haoenlu <haoenlu.07@gmail.com>
2024-08-17 18:14:29 -0400

    update

--- pipeline.py
+++ pipeline.py
@@ -51,14 +51,20 @@ scaler = FeatureWiseScaler(feature_range=(0,1))
 augmenter = WindowWarping(window_ratio=0.2,scales=[0.1,0.5,1,1.5,2,2.5])
 tasks = train_dataset.keys()
 
-all_data = []
-all_data_aug = []
-all_label = []
-all_label_aug = []
+all_train_data = []
+all_train_data_aug = []
+all_train_label = []
+all_train_label_aug = []
+all_test_data = []
+all_test_label = []
 for task in tqdm(tasks):
     train_data = np.concatenate(train_dataset[task],axis=0)
     train_data = scaler.fit_transform(train_data)
     train_data_aug = augmenter.generate(train_data)
+
+    test_data = np.concatenate(test_dataset[task],axis=0)
+    test_data = scaler.fit_transform(test_data)
+
 
     # plot_sample(train_data,train_data_aug,output_dir)
     # plot_pca(train_data,train_data_aug,output_dir)
@@ -70,18 +76,26 @@ for task in tqdm(tasks):
 
     # TODO: generate dataset with label
     label = np.argwhere(tasks == task)
-    all_data.append(train_data)
-    all_label.append([label] * train_data.shape[0])
-    all_data_aug.append(train_data_aug)
-    all_label_aug.append([label] * train_data_aug.shape[0])
+    all_train_data.append(train_data)
+    all_train_label.append([label] * train_data.shape[0])
+    all_train_data_aug.append(train_data_aug)
+    all_train_label_aug.append([label] * train_data_aug.shape[0])
+    all_test_data.append(test_data)
+    all_test_label.append([label] * test_data.shape[0])
 
-all_data = np.concatenate(all_data,axis=0)
-all_label = np.concatenate(all_label,axis=0)
-all_data_aug = np.concatenate(all_data_aug,axis=0)
-all_label_aug = np.concatenate(all_label_aug,axis=0)
+all_train_data = np.concatenate(all_train_data,axis=0)
+all_train_label = np.concatenate(all_train_label,axis=0).squeeze()
+all_train_data_aug = np.concatenate(all_train_data_aug,axis=0)
+all_train_label_aug = np.concatenate(all_train_label_aug,axis=0).squeeze()
+all_test_data = np.concatenate(all_test_data,axis=0)
+all_test_label = np.concatenate(all_test_label,axis=0).squeeze()
+
+print(all_train_data.shape,all_train_label.shape,all_train_data_aug.shape,all_train_label_aug.shape,all_test_data.shape,all_test_label.shape)
 
 
-print(all_data.shape,all_label.shape,all_data_aug.shape,all_label_aug.shape)
+plot_pca(all_train_data,all_test_data,output_dir)
+plot_tsne(all_train_data,all_test_data,output_dir)
+plot_umap(all_train_data,all_test_data,output_dir)
 
 # TODO: train classification on augmentation and original dataset and test with test dataset
 
