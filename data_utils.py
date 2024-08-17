1cda8d19ecef44a3080001fe6fd67840b6be4557
haoenlu <haoenlu.07@gmail.com>
2024-08-17 01:42:49 -0400

    update

--- data_utils.py
+++ data_utils.py
@@ -58,8 +58,10 @@ class TimeWarping:
         warped_window = np.interp(np.linspace(0, window_size, int(window_size*scale)), np.arange(window_size), time_series[start: start + window_size])
         print(warped_window.shape)
         # Replace the original window with the warped window
-        warped_series = np.append(time_series[:start],warped_window ,time_series[start+window_size:])
+        warped_series = np.append(time_series[:start], warped_window)
+        warped_series = np.append(warped_series,time_series[start+window_size:])
 
+        print(warped_series.shape)
         # Rescale the entire series to maintain the original length
         warped_series = np.interp(np.arange(n), np.linspace(0, n, n), warped_series)
 
