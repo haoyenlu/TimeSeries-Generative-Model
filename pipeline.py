f62b1b22bcfeed53c32e1003cb0e1783c75caa43
haoenlu <haoenlu.07@gmail.com>
2024-09-06 12:59:27 -0400

    update

--- pipeline.py
+++ pipeline.py
@@ -12,6 +12,7 @@ from data_utils import FeatureWiseScaler , WindowWarping, MovingAverageFilter
 from analysis_utils import plot_pca, plot_tsne, plot_umap, plot_sample, plot_confusion_matrix
 from dataset import ULF_Classification_Dataset, ULF_Generative_Dataset
 from train_utils import LinearLrDecay
+from train import train_generative_model, train_classificaton_model
 
 from sklearn.metrics import accuracy_score, f1_score
 from tqdm import tqdm
@@ -28,7 +29,7 @@ parser.add_argument('--data',type=str)
 parser.add_argument('--test_patient','-tp',nargs='+',type=str)
 parser.add_argument('--ih',action='store_true',help="Include Healthy Patient")
 parser.add_argument('--cc',type=str, help="Classification Model Config") 
-parser.add_argument('--gc',type=str, help="Generative Model Config")
+parser.add_argument('--gc',type=str, help="Generative Model Config",default=None)
 parser.add_argument('--max_ci',type=int, help="Max Classification Model Training Iteration")
 parser.add_argument('--max_gi',type=int, help="Max Generative Model Training Iteration")
 parser.add_argument('--save_gi',type=int, help="Generative Model Save Iteration")
@@ -51,7 +52,11 @@ os.makedirs(ckpt_dir,exist_ok=True)
 
 # CONFIG
 cc_config = load_config(args.cc)
-gc_config = load_config(args.gc)
+
+if args.gc:
+    gc_config = load_config(args.gc)
+else:
+    gc_config = None
 
 # logger
 # writer = SummaryWriter(log_dir)
@@ -91,8 +96,6 @@ def main(TEST_PATIENT):
     all_test_data = []
     all_test_label = []
 
-    g_trainer = get_trainer_from_config(gc_config)
-    g_trainer.save_weight(os.path.join(ckpt_dir,'initial_g.pth'))
 
     with tqdm(total=len(train_dataset.keys())) as pbar:
         for task in train_dataset.keys():
@@ -109,12 +112,7 @@ def main(TEST_PATIENT):
 
             # TODO: train generative model on train_data for augmentation
             logger.info(f"Training Generative Model on {task}")
-            g_trainer.load_weight(os.path.join(ckpt_dir,'initial_g.pth'))
-            scheduler = LinearLrDecay(g_trainer.optimizer, gc_config['optimizer']['lr'], 0.0, 0, args.max_gi)
-            train_generative_dataset = ULF_Generative_Dataset(train_data)
-            train_generative_dataloader = DataLoader(train_generative_dataset) 
-            g_trainer.train(train_generative_dataloader,args.max_gi,args.save_gi,scheduler,writer,verbal = args.verbal, save_path=os.path.join(ckpt_dir,TEST_PATIENT,task,'best_generative_weight.pth'))
-            samples = g_trainer.generate_samples(num_samples=train_data.shape[0],num_per_batch=10)
+            samples = train_generative_model(gc_config,train_data,args.max_gi,args.save_gi,args.verbal,ckpt_dir=os.path.join(ckpt_dir,TEST_PATIENT,task))
             samples = filter.apply(samples)
             all_train_data_aug_diffusion.append(samples)
 
@@ -145,53 +143,32 @@ def main(TEST_PATIENT):
     all_train_label_aug = np.squeeze(np.concatenate(all_train_label_aug,axis=0))
     all_test_data = np.concatenate(all_test_data,axis=0)
     all_test_label = np.squeeze(np.concatenate(all_test_label,axis=0))
-    all_train_data_aug_diffusion = np.concatenate(all_train_data_aug_diffusion,axis=0)
-    all_train_label_aug_diffusion = np.concatenate(all_train_label_aug_diffusion,axis=0).squeeze()
 
-    print(all_train_data_aug_diffusion.shape)
+    if len(all_train_data_aug_diffusion) != 0 and len(all_train_label_aug_diffusion) != 0:
+        all_train_data_aug_diffusion = np.concatenate(all_train_data_aug_diffusion,axis=0)
+        all_train_label_aug_diffusion = np.concatenate(all_train_label_aug_diffusion,axis=0).squeeze()
+        print(all_train_data_aug_diffusion.shape)
 
 
     # TODO: train classification on augmentation and original dataset and test with test dataset
-    train_dataset = ULF_Classification_Dataset(all_train_data,all_train_label)
-    test_dataset = ULF_Classification_Dataset(all_test_data,all_test_label)
-    train_dataloader = DataLoader(train_dataset,cc_config['batch_size'],shuffle=True)
-    test_dataloader = DataLoader(test_dataset,cc_config['batch_size'],shuffle=True)
-
-    # IMPORT CLASSIFICATION MODEL
     # train without augmentation
     logger.info("Train without Augmentation")
-    trainer  = get_trainer_from_config(cc_config)
-    trainer.save_weight(os.path.join(ckpt_dir,"initial.pth"))
-    trainer.train(train_dataloader,test_dataloader,args.max_ci,os.path.join(ckpt_dir,f'{TEST_PATIENT}_best.pth'),verbal=args.verbal,writer=writer)
-    trainer.load_weight(os.path.join(ckpt_dir,f'{TEST_PATIENT}_best.pth'))
-    prediction = trainer.make_prediction(all_test_data)
+    prediction = train_classificaton_model(cc_config,all_train_data,all_train_label,all_test_data,all_test_label,args.max_ci,args.verbal,os.path.join(ckpt_dir,TEST_PATIENT,'Original'))
     plot_confusion_matrix(all_test_label, prediction, output_dir, title=f"{TEST_PATIENT}-Original-Prediction")
-
     orig_acc, orig_f1 = accuracy_score(all_test_label,prediction), f1_score(all_test_label,prediction,average='micro')
     logger.info(f"{TEST_PATIENT}(WITHOUT AUGMENTATION): Accuracy: {orig_acc*100:.2f}% | F1-score: {orig_f1*100:.2f}%")
 
     # train with augmentation
-    logger.info("Train with Time Warping Augmentation")
-    train_dataset = ULF_Classification_Dataset(np.concatenate([all_train_data,all_train_data_aug],axis=0),np.concatenate([all_train_label,all_train_label_aug],axis=0))
-    train_dataloader = DataLoader(train_dataset,cc_config['batch_size'],shuffle=True)
-    trainer.load_weight(os.path.join(ckpt_dir,"initial.pth"))
-    trainer.train(train_dataloader,test_dataloader,args.max_ci,os.path.join(ckpt_dir,f'{TEST_PATIENT}_best_aug.pth'),verbal=args.verbal,writer=writer)
-    trainer.load_weight(os.path.join(ckpt_dir,f'{TEST_PATIENT}_best_aug.pth'))
-    prediction = trainer.make_prediction(all_test_data)
+    logger.info("Train with Time Warping Augmentation") 
+    prediction = train_classificaton_model(cc_config,np.concatenate([all_train_data,all_train_data_aug],axis=0),np.concatenate([all_train_label,all_train_label_aug],axis=0),all_test_data,all_test_label,args.max_ci,args.verbal,os.path.join(ckpt_dir,TEST_PATIENT,'TW-Augmentation'))
     plot_confusion_matrix(all_test_label,prediction,output_dir,title=f"{TEST_PATIENT}-TW-Augmented-Prediciton")
     aug_acc, aug_f1 = accuracy_score(all_test_label,prediction), f1_score(all_test_label,prediction,average="micro")
     logger.info(f"{TEST_PATIENT}(WITH Time-Warping AUGMENTATION): Accuracy: {aug_acc*100:.2f}% | F1-score: {aug_f1*100:.2f}%")
 
     # train with diffusion augmentation
     logger.info("Train with Diffusion Augmentation")
-    train_dataset = ULF_Classification_Dataset(np.concatenate([all_train_data,all_train_data_aug_diffusion],axis=0),np.concatenate([all_train_label,all_train_label_aug_diffusion],axis=0))
-    train_dataloader = DataLoader(train_dataset,cc_config['batch_size'],shuffle=True)
-    trainer.load_weight(os.path.join(ckpt_dir,"initial.pth"))
-    trainer.train(train_dataloader,test_dataloader,args.max_ci,os.path.join(ckpt_dir,f'{TEST_PATIENT}_best_aug.pth'),verbal=args.verbal,writer=writer)
-    trainer.load_weight(os.path.join(ckpt_dir,f'{TEST_PATIENT}_best_aug.pth'))
-    prediction = trainer.make_prediction(all_test_data)
+    prediction = train_classificaton_model(cc_config,np.concatenate([all_train_data,all_train_data_aug_diffusion],axis=0),np.concatenate([all_train_label,all_train_label_aug_diffusion],axis=0),all_test_data,all_test_label,args.max_ci,args.verbal,os.path.join(ckpt_dir,TEST_PATIENT,'Diffusion-Augmentation'))
     plot_confusion_matrix(all_test_label,prediction,output_dir,title=f"{TEST_PATIENT}-Diffusion-Augmented-Prediciton")
-
     diffusion_aug_acc, diffusion_aug_f1 = accuracy_score(all_test_label,prediction), f1_score(all_test_label,prediction,average="micro")
     logger.info(f"{TEST_PATIENT}(WITH Diffusion AUGMENTATION): Accuracy: {diffusion_aug_acc*100:.2f}% | F1-score: {diffusion_aug_f1*100:.2f}%")
     
--- train.py
+++ train.py
@@ -19,169 +19,27 @@ from data_utils import FeatureWiseScaler
 from tqdm import tqdm
 
 
+def train_generative_model(config,data,max_iter,save_iter,verbal,ckpt_dir):
+    os.makedirs(ckpt_dir,exist_ok=True)
+    trainer = get_trainer_from_config(config)
+    scheduler = LinearLrDecay(trainer.optimizer,config['optimizer']['lr'],0.0,0,max_iter)
+    dataset = ULF_Generative_Dataset(data)
+    dataloader = DataLoader(dataset)
+    trainer.train(dataloader,max_iter,save_iter,scheduler,writer=None,verbal=verbal,save_path=os.path.join(ckpt_dir,'best_weight.pth'))
+    samples = trainer.generate_samples(num_samples=data.shape[0],num_per_batch=10)
 
-def train_generative(_config):
-    config = load_config(args.config)
-    curr_date = datetime.now().strftime("%d%m%Y_%H%M%S")
-
-    
-    # folder path
-    checkpoint_path = os.path.join(args.ckpt,curr_date) # checkpoint
-    log = os.path.join(args.log,curr_date) # log
-    output = os.path.join(args.save,curr_date) # save folder path
-
-    os.makedirs(checkpoint_path,exist_ok=True)
-    os.makedirs(log,exist_ok=True)
-    os.makedirs(output,exist_ok=True)
-
-    # file name
-    initial_setting = "initial_setting.pth"
-    best_weight = "best_weight.pth"
+    return samples
 
 
-    # load ulf data
-    data = np.load(args.data, allow_pickle=True).item()
-
-    # save the initial setting - reused trainer for training other task
-    trainer = get_trainer_from_config(args,config)
-    trainer.save_weight(os.path.join(checkpoint_path,initial_setting))
-
-    for task in args.task:
-        print(f"---- Training on {task} ------")
-
-        # Load Data
-        train_dataset = ULF_Generative_Dataset(data[task])
-        train_dataloader = DataLoader(train_dataset,config['batch_size'],shuffle=True) 
-        
-
-        # logger
-        if args.log is not None:
-            os.makedirs(os.path.join(log,task),exist_ok=True)
-            writer = SummaryWriter(log)
-        else:
-            writer = None
-
-        # load initial setting before training - load weight before instantiate scheduler
-        trainer.load_weight(os.path.join(checkpoint_path,initial_setting))
-
-        # Learning rate scheduler
-        scheduler = LinearLrDecay(trainer.optimizer,config['optimizer']['lr'],0.0,0,args.max_iter)
-
-
-        # training
-        os.makedirs(os.path.join(checkpoint_path,task),exist_ok=True)
-        trainer.train(train_dataloader,args.max_iter,args.save_iter,scheduler,writer,verbal = args.verbal, save_path=os.path.join(checkpoint_path,task,best_weight))
-
-
-        # generate samples
-        samples = trainer.generate_samples(num_samples=100,num_per_batch=10)
-
-        save_folder = os.path.join(output,task)
-        os.makedirs(save_folder,exist_ok=True)
-        np.save(os.path.join(save_folder,'synthesize.npy'), samples)
-
-        # analyze with pca and tsne
-        train_data = train_dataset._get_numpy()
-
-        plot_pca(real=train_data,fake=samples,save_path=save_folder)
-        plot_tsne(real=train_data,fake=samples,save_path=save_folder)
-        plot_umap(real=train_data,fake=samples,save_path=save_folder)
-        plot_sample(real=train_data,fake=samples,save_path=save_folder)
-
-
-
-def train_classification():
-    args = train_classification_argument()
-    config = load_config(args.config)
-
-    curr_date = datetime.now().strftime("%d%m%Y_%H%M%S")
-    
-    # folder path
-    checkpoint_path = os.path.join(args.ckpt,curr_date) # checkpoint
-    log = os.path.join(args.log,curr_date) # log
-    output = os.path.join(args.save,curr_date) # save folder path
-
-    os.makedirs(checkpoint_path,exist_ok=True)
-    os.makedirs(log,exist_ok=True)
-    os.makedirs(output,exist_ok=True)
-
-    # logger
-    if args.log is not None:
-        os.makedirs(log,exist_ok=True)
-        writer = SummaryWriter(log)
-    else:
-        writer = None
-
-    best_original_weight = 'best_original_weight.pth'
-    best_augmentation_weight = 'best_augmentation_weight.pth'
-    initial_weight = 'initial_weight.pth'
-
-    train_data, train_label = preprocess_data(args.train_data)
-    test_data, test_label = preprocess_data(args.test_data)
-    
-    
+def train_classificaton_model(config,train_data,train_label,test_data,test_label,max_iter,verbal,ckpt_dir):
+    os.makedirs(ckpt_dir,exist_ok=True)
+    trainer = get_trainer_from_config(config)
     train_dataset = ULF_Classification_Dataset(train_data,train_label)
+    train_dataloader = DataLoader(train_dataset,batch_size=config['batch_size'],shuffle=True)
     test_dataset = ULF_Classification_Dataset(test_data,test_label)
-    train_dataloader = DataLoader(train_dataset,config['batch_size'],shuffle=True)
-    test_dataloader = DataLoader(test_dataset,config['batch_size'],shuffle=True)
+    test_dataloader = DataLoader(test_dataset,batch_size=config['batch_size'],shuffle=True)
+    trainer.train(train_dataloader,test_dataloader,max_iter,os.path.join(ckpt_dir,'best_weight.pth'),verbal=verbal,writer=None)
+    trainer.load_weight(os.path.join(ckpt_dir,'best_weight.pth'))
+    prediction = trainer.make_prediction(test_data)
 
-
-
-    '''Train without augmentation'''
-    print("Start training without Augmentation.")
-    trainer = get_trainer_from_config(args,config)
-    trainer.save_weight(os.path.join(checkpoint_path,initial_weight))
-    trainer.train(train_dataloader,test_dataloader,args.max_iter,writer,os.path.join(checkpoint_path,best_original_weight))
-    trainer.load_weight(os.path.join(checkpoint_path,best_original_weight))
-    predictions = trainer.make_prediction(test_data)
-    plot_confusion_matrix(test_label,predictions,output,title="Original_Prediction")
-
-
-    '''Train with augmentation'''
-    if args.aug_data is not None:
-        print("Start training with Augmentation.")
-        aug_data, aug_label = preprocess_data(args.aug_data,scale=False)
-        train_data = np.concatenate([train_data,aug_data],axis=0)
-        train_label = np.concatenate([train_label,aug_label],axis=0)
-
-        train_dataset = ULF_Classification_Dataset(train_data,train_label)
-        train_dataloader = DataLoader(train_dataset,config['batch_size'],shuffle=True)
-        trainer.load_weight(os.path.join(checkpoint_path,initial_weight)) # load initial weight
-        trainer.train(train_dataloader,test_dataloader,args.max_iter,writer,os.path.join(checkpoint_path,best_augmentation_weight))
-        trainer.load_weight(os.path.join(checkpoint_path,best_augmentation_weight))
-        predictions = trainer.make_prediction(test_data)
-        plot_confusion_matrix(test_label,predictions,output,title="Augmented_Prediction")
-
-
-
-
-
-
-def preprocess_data(data_path,scale=True):
-    data_dict = np.load(data_path,allow_pickle=True).item()
-    tasks = np.array(list(data_dict.keys()))
-    data = []
-    label = []
-
-    scaler = FeatureWiseScaler((0,1))
-
-    pbar = tqdm(total=len(data_dict.items()))
-    for key, value in data_dict.items():
-        np_value = np.array(value)
-        B , T, C = np_value.shape
-        if scale: np_value = scaler.fit_transform(np_value)
-        data.append(np_value)
-        l = np.argwhere(tasks == key)
-        label.append([l] * B)
-        pbar.update(1)
-    
-    data = np.concatenate(data)
-    label = np.concatenate(label).squeeze()
-
-    return data, label
-
-
-
-
-if __name__ == '__main__':
-    train_generative()
+    return prediction
